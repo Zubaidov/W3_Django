fb734ce44a09287d9c505c2d3bc013600ea70788
Zubaidov <zubaidov.shohruz@gmail.com>
2023-12-09 16:17:53 +0400

    Template + View + Model

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members')
+    path('members/', views.members, name='members'),
+    path('all_members/', views.all_members, name='all_members')
 ]
--- members/views.py
+++ members/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def members(request):
+    mymembers = Members.objects.all()
     template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    context = {
+        'mymembers':mymembers,
+    }
+    return render(request, 'myfirst.html', context)
+
+def all_members(request):
+    mymembers = Members.objects.all()
+    context={
+        'mymembers': mymembers,
+    }
+    for x in mymembers:
+        print(x.firstname, x.lastname, x.age)
+    return render(request, 'all_members.html', context)
